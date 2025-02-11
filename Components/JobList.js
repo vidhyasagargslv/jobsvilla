ccd8d35f502b0d8e67f4b1a9279a9978e9255d3b
vidhya sagar <vidhyasagargslv@gmail.com>
2025-02-11 18:51:12 +0530

    Implimented sliding card

--- Components/JobList.js
+++ Components/JobList.js
@@ -1,81 +1,141 @@
-import { Briefcase, MapPin, DollarSign, GraduationCap, Clock, Phone, Users } from "lucide-react"
+import React, { useState } from 'react';
+import { Landmark, MapPin,ListCollapse, DollarSign, GraduationCap,CalendarSearch, Phone, Users, X } from "lucide-react";
+
 export default function JobList({ jobs }) {
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {jobs.map((job) => (
-          <div
-            key={job.id}
-            className="bg-card border border-gray-50 p-6 rounded-lg shadow-lg h-[32rem] flex flex-col"
-          >
-            {/* Job Title */}
-            <h2 className="text-2xl font-bold text-foreground line-clamp-2 min-h-[4rem]">
-
-              {job.title}
-            </h2>
-
-            {/* Company and Location */}
-            <p className="text-muted-foreground flex flex-col ">
-              <span className="company inline-flex items-center line-clamp-1">
-                <Briefcase className="inline-block" size={24} />
-                 {job.company}
-              </span>
-              <span className="location inline-flex items-center mt-1 line-clamp-1">
-                <MapPin className="inline-block" size={24} />
-                 {job.location}
-              </span>
-            </p>
-
-            {/* Job Category and Employment Type */}
-            <div className="mt-4 flex flex-wrap gap-2">
-              <span className="bg-secondary text-secondary-foreground px-3 py-1 rounded-full text-sm text-white">
-                {job.job_category}
-              </span>
-              <span className="bg-secondary text-secondary-foreground px-3 py-1 rounded-full text-sm">
-                {job.employment_type}
-              </span>
-            </div>
-
-            {/* Salary Range */}
-            <p className="mt-4 text-lg font-semibold text-foreground">
-              <span><DollarSign className="inline-block" size={20} /></span>
-              {job.salary_from.toLocaleString()} - {job.salary_to.toLocaleString()} per year
-            </p>
-
-            {/* Qualifications */}
-            <div className="mt-4 flex-grow overflow-hidden">
-              <h3 className="font-semibold text-blue-500">
-              <GraduationCap className="inline-block" size={24} />
-                Qualifications
-              </h3>
-              <ul className="list-disc list-inside text-muted-foreground mt-2">
-                {JSON.parse(job.qualifications).slice(0, 3).map((qualification, index) => (
-                  <li key={index} className="line-clamp-1">
-                    
-                    {qualification}
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            {/* Contact Information */}
-            <p className="mt-4 text-muted">
-              <span className="inline-flex items-center text-black line-clamp-1">
-                <Phone className="inline-block" size={24} />
-                 Contact: {job.contact}
-              </span>
-            </p>
-
-            {/* Number of Openings */}
-            <div className="mt-4">
-              <span className="text-muted text-black">
-                <Users className="inline-block" size={24} />
-                {job.number_of_opening} opening(s)
-              </span>
-            </div>
-          </div>
+          <JobCard key={job.id} job={job} />
         ))}
       </div>
     </div>
   );
 }
+
+const JobCard = ({ job }) => {
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
+
+  const getDeadlineTextClass = (deadline) => {
+    const deadlineDate = new Date(deadline);
+    const now = new Date();
+    return deadlineDate < now ? "text-danger" : "text-success";
+  };
+
+  return (
+    <div className=" border p-6 rounded-lg shadow-lg h-[20rem] flex flex-col transition-shadow relative overflow-hidden">
+      {/* Job Title */}
+      <p className="text-blue-600 text-2xl font-bold line-clamp-2 min-h-[4rem]">
+        {job.title}
+      </p>
+
+      {/* Company and Location */}
+      <div className="flex flex-col ">
+        <div className="flex flex-row items-center gap-2">
+          
+          <Landmark className="w-4 h-4 mr-2" />
+          <span className="mr-4">{job.company}</span>
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <MapPin className="w-4 h-4 mr-2" />
+          <span>{job.location}</span>
+        </div>
+      </div>
+
+      {/* Job Category and Employment Type */}
+      <div className="mt-4 flex flex-wrap gap-2">
+        <span className="badge badge-outline">
+          {job.job_category}
+        </span>
+        
+      </div>
+
+      {/* Salary Range */}
+      <div className="flex items-center  text-md mt-4">
+        <DollarSign className="w-4 h-4 mr-5" />
+        
+
+        <span>
+          ${job.salary_from.toLocaleString()} - ${job.salary_to.toLocaleString()} per year
+        </span>
+      </div>
+
+      
+
+      {/* Contact Information */}
+      <div className="flex items-center">
+        <CalendarSearch className="w-4 h-4 mr-5" />
+        <span className='font-bold'>Deadline:  </span>
+        <span className={getDeadlineTextClass(job.application_deadline)}>{job.application_deadline}</span>
+      </div>
+
+      {/* Number of Openings */}
+      <div className="flex items-center ">
+        <CalendarSearch className="w-4 h-4 mr-5" />
+        <span>Posted On: {job.created_at}</span>
+      </div>
+
+      {/* View More Button */}
+      <div className="mt-4 flex">
+        <button
+          onClick={() => setIsDetailOpen(true)}
+          className="btn btn-info btn-outline btn-sm"
+        >
+          View More
+        </button>
+      </div>
+      
+
+      {/* Sliding Panel */}
+      <div
+        className={`absolute inset-x-0 bottom-0 bg-black shadow-lg transition-transform duration-300 ease-in-out transform ${
+          isDetailOpen ? "translate-y-0" : "translate-y-full"
+        }`}
+        style={{ height: "80%" }}
+      >
+        <div className="p-6 h-full overflow-y-auto">
+          <button
+            className="absolute top-2 right-2 p-2 rounded-full bg-gray-600 hover:bg-gray-100"
+            onClick={() => setIsDetailOpen(false)}
+          >
+            <X className="h-4 w-4 " />
+          </button>
+          
+          <h3 className="font-bold text-lg mb-2 text-cyan-600">
+          <ListCollapse className="w-4 h-4 mr-5" />
+            Description</h3>
+          <p className="text-slate-100 mb-4">{job.description}</p>
+          
+          <h3 className="font-bold text-lg mb-2 flex items-center text-cyan-600 ">
+            <GraduationCap className="w-4 h-4 mr-2" />
+            Qualifications
+          </h3>
+          <ul className="list-disc list-inside text-sm mb-4 text-slate-100">
+            {JSON.parse(job.qualifications).map((qual, index) => (
+              <li key={index}>{qual}</li>
+            ))}
+          </ul>
+
+          <div className="flex flex-wrap gap-2 mb-4 text-slate-100">
+            <span className="badge badge-outline">
+              {job.job_category}
+            </span>
+            <div className="badge badge-outline">
+              {job.employment_type}
+            </div>
+          </div>
+
+          <div className="flex items-center  mb-2 text-slate-100">
+            <Phone className="w-4 h-4 mr-2" />
+            <span>Contact: {job.contact}</span>
+          </div>
+
+          <div className="flex items-center text-slate-100">
+            <Users className="w-4 h-4 mr-2" />
+            <span>{job.number_of_opening} openings</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
